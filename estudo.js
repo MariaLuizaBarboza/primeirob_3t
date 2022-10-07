524940e3c97772f156454bf4fbe13a959ff24b37
MariaLuizaBarboza <108934750+MariaLuizaBarboza@users.noreply.github.com>
2022-10-07 09:09:13 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,33 +1,48 @@
-let tabuada = 2;
-function escreva (){
-     document.write (" Tabuada do" + tabuada + "<br>")
-      document.write (tabuada + " x 1 =" + (tabuada*1)+ "<br>");
-      document.write (tabuada + " x 2 =" + (tabuada*2)+ "<br>");
-      document.write (tabuada + " x 3 =" + (tabuada*3)+ "<br>");
-      document.write (tabuada + " x 4 =" + (tabuada*4)+ "<br>");
-      document.write (tabuada + " x 5 =" + (tabuada*5)+ "<br>");
-      document.write (tabuada + " x 6 =" + (tabuada*6)+ "<br>");
-      document.write (tabuada + " x 7 =" + (tabuada*7)+ "<br>");
-      document.write (tabuada + " x 8 =" + (tabuada*8)+ "<br>");
-      document.write (tabuada + " x 9 =" + (tabuada*9)+ "<br>");
-      document.write (tabuada + " x 10 =" + (tabuada*10)+ "<br>");
-}
- 
-let lista = ["Jesus", "Spessato", "Duarte", "Jorge", "Neuza", "Gilmar", "Jossyara"];
-function mostrarLista (){
-      document.write("Tamanho da Lista:" + lista.length + "<br>");
-      for(let i=0; i <lista.length; i++) {
-            document.write("Professor:" + lista[i] + "<br>");
-      }
-     }
+let tabuada = 111;
 
- function multiplicar (){
-      for(let i = 1 <= 10; i++){
-            document.write("O valor do i" = i+ "<br>");
-            for( let j = 1; <= 10; j++) {
-                  document.write(i + "x"+j+" = " + (tabuada*1) + "<br>")
-            }
+function escreva(){
+document.write(" Tabuada do " + tabuada + "<br>");    
+document.write(tabuada + " X 1 = " + (tabuada*1) + "<br>");
+document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
+document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
+document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
+document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
+document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
+document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
+document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
+}
+
+let lista = ["gilzinho","spessato","neuza","jesus","Duarte"];
+
+function mostraLista(){
+    document.write("tamanho da lista: "  +  lista.length + "<br>");
+    for(let i = 0; i <lista.length; i++){
+        document.write("professores: " + lista[i] + "<br>")
+    }
+}
+
+function multiplica (){
+      for(let i = 1; i <= 10; i++){
+      document.write("Taboada do = " + i + "<br>");
+      for(let j = 1; j <=10; j++){
+        document.write(i + " x"+j+ " = " + (i*j) + "<br>");
+
+      }
+      document.write("<br>")
+  }
+}
+function total(){
+  let  v = document.getElementById("Valor").value;
+  let  j = document.getElementById("Juros").value;
+  let  t = document.getElementById("Meses").value; 
+  let  r = 0;
+for (let i=1; i <= t;i++) {
+  r = v * (1+(j/100));
+  document.write("Mês "+ i + "valor: " + r + "<br>");
+  v = r; 
 
 }
-      }
- 
+  document.write("Resultado: "+ r);
+}
